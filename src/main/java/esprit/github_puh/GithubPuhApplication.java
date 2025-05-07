e00e9cc7542b26d648c38175ad9f9567a39983f1
salmabm <salma.benmiled@esprit.tn>
2025-05-07 21:39:50 +0100

    first commit

---
+++ src/main/java/esprit/github_puh/GithubPuhApplication.java
@@ -0,0 +1,13 @@
+package esprit.github_puh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPuhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubPuhApplication.class, args);
+    }
+
+}
